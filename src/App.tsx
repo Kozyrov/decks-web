7f1906efc12e2ef4a5566383c101af7d8156d3cf
Kozyrov <sfrankie11@gmail.com>
2022-12-13 02:44:13 -0800

    added new compnent to house blade menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Deck from './components/Deck/Deck';
-import './App.module.scss';
+import BladeMenuContainer from './components/BladeMenu/BladeMenuContainer';
+import styles from './App.module.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Deck />
+      <header className="App_header">
       </header>
+      <div className={styles.App_body}>
+        <BladeMenuContainer />
+        <Deck />
+      </div>
     </div>
   );
 }
---
+++ src/components/BladeMenu/StaticMenu.tsx
@@ -0,0 +1,9 @@
+const StaticMenu = () => {
+    return (
+        <div>
+            this is the menu
+        </div>
+    )
+}
+
+export default StaticMenu;
